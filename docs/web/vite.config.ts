503a4d1b745d90d448fe51d45cc178fd452e32b0
Gustavo Hammes <daffa1632@gmail.com>
2025-09-12 21:42:07 -0300

    Configura projeto para deploy no GitHub Pages

--- docs/web/vite.config.ts
+++ docs/web/vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ra2581392423005/", // Adicionado para o GitHub Pages
+  base: "/ra2581392423005/",
+  build: {
+    outDir: "docs", //  👈 Adicione esta seção para mudar a pasta de saída
+  }, // Adicionado para o GitHub Pages
 });
